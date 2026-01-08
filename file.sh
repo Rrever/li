eb307e038ff786fb0769838f1a287fbe81e6b1ad
Rrever <dnakul007@gmail.com>
2026-01-09 01:57:32 +0530

    x

--- file.sh
+++ file.sh
@@ -1,28 +1,28 @@
-find_file_type()
-{
- if [ -e "$1" ]; then
-       echo "File in directory $1"
-       file "$1"
- else
-       echo "Directory doest not exist"
-       fi
+#!/bin/bash
+
+find_file_type() {
+    if [ -e "$1" ]; then
+        echo "File exists: $1"
+        file "$1"
+    else
+        echo "File does not exist"
+    fi
 }
 
-list_files()
-{
- if [ -d "$1" ]; then
-          echo "files in directory $1"
-          ls -l "$1"
- else
-          echo "Directory does not exist"
-          fi
+list_files() {
+    if [ -d "$1" ]; then
+        echo "Files in directory: $1"
+        ls -l "$1"
+    else
+        echo "Directory does not exist"
+    fi
 }
 
-echo "Enter the directory to list files"
-read dir 
+echo "Enter the directory to list files:"
+read dir
 
-echo "enter the file name to check type"
+echo "Enter the file name (with path) to check type:"
 read filename
 
 list_files "$dir"
-find_file_type "$filename"           
+find_file_type "$filename"
